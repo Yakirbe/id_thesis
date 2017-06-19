9e89fafcff4033420920eb4eaba6d32ce40184a5
yakir <yaqir1@gmail.com>
2017-06-19 10:41:42 +0300

    ..

--- python_id/colordiff/main.py
+++ python_id/colordiff/main.py
@@ -5,7 +5,7 @@ Created on Wed Feb 22 16:17:49 2017
 """
 
 import json
-from prepare_ds import prep_ds
+from prepare_ds import prep_ds, de_pair
 from edit_x_tr import arrange_x
 from id_color_diff_cam import embed_main, arrange_ds
 from train_test_sgd import train , test
@@ -34,19 +34,19 @@ def run_idd(cam = False, fw = True, c = ""):
     X_tr , X_te , Y_tr , Y_te = arrange_x(cam = cam)
     #return X_tr , X_te , Y_tr , Y_te
     # Calculate coefficients
-    l_rate = 0.01
-    n_epoch = 5
+    l_rate = 0.1
+    n_epoch = 50
     w = train(X_tr,Y_tr, l_rate, n_epoch, weight = "", reg = "")
     errors = test(X_te, Y_te, w, method = "L2")
     print "cam = ", cam
     print "c = ", c
-    return errors
+    return {"err":errors,"c":c}
 
 if __name__ == "__main__":
     out = {"cam":{}, "color":{}}
     #for _ in range(10):
-    for c in range(2,3):
+    for c in range(2,9):
         out["color"][c] = run_idd(cam = False, fw = True, c = c)
-#    for c in range(2,3):
-#        out["cam"][c] = run_idd(cam = True, fw = True, c = c)
+    for c in range(2,9):
+        out["cam"][c] = run_idd(cam = True, fw = True, c = c)
         
--- python_id/colordiff/train_test_sgd.py
+++ python_id/colordiff/train_test_sgd.py
@@ -8,7 +8,7 @@ Created on Mon Mar  6 23:38:13 2017
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import random
 import  numpy as np
-
+from prepare_ds import de_pair
 
 def L2(pair):
     v1 = pair[:3]
@@ -24,9 +24,9 @@ def L1(pair):
     
 # Make a prediction with coefficients
 def predict(row, coefficients):
-    yhat = coefficients[0]
+    yhat = coefficients[0] * row[0]
     for i in range(len(row)-1):
-        yhat += coefficients[i + 1] * row[i]
+        yhat += coefficients[i + 1] * row[i + 1]
     return yhat
 
 def gen_ds(d_num = 5000, n_feat = 100):
@@ -45,8 +45,8 @@ def train(tr_data, labels, l_rate, n_epoch, weight = "", reg = ""):
     else:
         coef = [0.0 for i in range(len(tr_data[0])-6)]
     for epoch in range(n_epoch):
-#        if epoch > 500:
-#            l_rate = l_rate*0.1
+#        if epoch%500 == 0:
+#            l_rate = l_rate*0.5
         sum_error = 0
         index = 0
         for row in tr_data:
@@ -54,17 +54,17 @@ def train(tr_data, labels, l_rate, n_epoch, weight = "", reg = ""):
             pair = row[:6]
             #print pair
             emb_vec = row[6:]
-            yhat = predict(emb_vec, coef)
+            yhat = np.dot(emb_vec, coef)
             if not reg:
-                rg = L2(pair) 
-            error = yhat - l + rg
+                #rg = L1(pair) 
+                rg = de_pair(pair)
+            error = yhat - l + 0.01*rg
+            #print "error = ", error, "rg = " , rg, "pred = ", yhat, "true = ", l
             sum_error += error**2
-            coef[0] = coef[0] - l_rate * error *emb_vec[0]
-            for i in range(len(emb_vec)-1):
-                #coef[i + 1] = np.max(0.0,coef[i + 1] - l_rate * error * emb_vec[i])
-                coef[i + 1] =coef[i + 1] - l_rate * error * emb_vec[i]
+            for i in range(len(emb_vec)):
+                coef[i] =coef[i] - l_rate * error * emb_vec[i]
             index += 1
-        #print('epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+        print('epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
         #print
     return coef
     
@@ -77,11 +77,12 @@ def test(test, labels, w, method = "L2"):
     for t in test:
         pair = t[:6]
         sample = t[6:]
-        y_pred = np.dot(sample, w)
+        rg = de_pair(pair)
+        y_pred = np.dot(sample, w) + 0.01*rg
         y_true = labels[i]
         yt_l.append(y_true)
         yp_l.append(y_pred)
-        #print "pred = ", y_pred , "true = ", y_true
+        print "pred = ", y_pred , "true = ", y_true, "l2 = " , L2(pair)
         i += 1
     print "mse =",  mean_squared_error(yt_l, yp_l)
     print "mae =",  mean_absolute_error(yt_l, yp_l)
@@ -94,6 +95,8 @@ if __name__ == "__main__":
     l_rate = 0.001
     n_epoch = 300
     #w = train(X_tr,Y_tr, l_rate, n_epoch)
-    l2_error = test(X_te, Y_te, w)
+    #l2_error = test(X_te, Y_te, w)
     #l2_error = test(X_tr, Y_tr, w)
-    print l2_error
+    
+    print predict([1,2,3],[1,2,4])
+    print np.dot([1,2,3],[1,2,4])
